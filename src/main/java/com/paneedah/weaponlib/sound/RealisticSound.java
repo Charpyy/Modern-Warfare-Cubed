4cafc534af1d989238eb1a9bbfd6c0c96d7cf629
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-12-06 12:51:16 +0100

    Update RealisticSound.java

--- src/main/java/com/paneedah/weaponlib/sound/RealisticSound.java
+++ src/main/java/com/paneedah/weaponlib/sound/RealisticSound.java
@@ -7,55 +7,37 @@ import net.minecraft.util.math.BlockPos;
 public class RealisticSound {
 
     private final SoundEvent sound;
-    private final SoundCategory categoryIn;
     private final float volumeIn;
     private final float pitchIn;
 
-    private static final float MINIMUM_VOLUME = 0.0F;
-    private static final float BASE_VOLUME = 1.0F;
-    private static final float BASE_PITCH = 1.0F;
-    private static final double MAX_DISTANCE = 3400.0F;
+    private static final double MAX_DISTANCE = 400D;
+    private static final double MAX_DISTANCE_SILENCER = 100D;
 
-    public RealisticSound(SoundEvent sound, SoundCategory categoryIn, float volumeIn, float pitchIn) {
+    public RealisticSound(SoundEvent sound, float volumeIn, float pitchIn) {
         this.sound = sound;
-        this.categoryIn = categoryIn;
         this.volumeIn = volumeIn;
         this.pitchIn = pitchIn;
     }
 
     public static RealisticSound createSound(Boolean silencer, SoundEvent soundIn, BlockPos origin, BlockPos playercoord) {
         double distance = Math.sqrt(playercoord.distanceSq(origin));
-        float volume = (float) adjustVolumeForDistance(distance, MAX_DISTANCE);
-        if (silencer && volume > 0D) {
-            volume *= 0.4F;
-        }
-        //float pitch = adjustPitchForDistance(distance);
-        return new RealisticSound(soundIn, SoundCategory.PLAYERS, volume, BASE_PITCH);
+        float volume = (float) adjustVolumeForDistance(silencer, distance);
+        return new RealisticSound(soundIn, volume, 1.0F);
     }
 
-
-    //DOPPLER
-    private static float adjustPitchForDistance(double distance) {
-        return BASE_PITCH * (1.0f - (float) Math.min(distance / 100.0, 0.5));
-    }
-
-
-    public static double adjustVolumeForDistance(double distance, double maxDistance) {
+    public static double adjustVolumeForDistance(boolean silencer, double distance) {
+        double maxDistance = silencer ? MAX_DISTANCE_SILENCER : MAX_DISTANCE;
         if (distance >= maxDistance) {
-            return 0.0F;
+            return 0.0;
         }
         if (distance <= 0) {
-            return 1.0F;
+            return 1.0;
         }
-        return 1.0F - (distance / maxDistance);
+        return 1.0 - (distance / maxDistance);
     }
 
     public SoundEvent getSound() {
         return sound;
-    }
-
-    public SoundCategory getCategoryIn() {
-        return categoryIn;
     }
 
     public float getVolumeIn() {
